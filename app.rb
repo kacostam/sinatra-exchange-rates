c92f63f1edc4e64ba317bbdf2591680414c3bb2a
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-06 05:32:36 +0000

    currencies as global variables and conversion tab

--- app.rb
+++ app.rb
@@ -1,39 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
 require "dotenv/load"
-
-# Pull in the HTTP class
 require "http"
 
-# define a route for the homepage
+# Fetch the currencies once and store in a global variable
+api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+raw_response = HTTP.get(api_url)
+parsed_data = JSON.parse(raw_response.to_s)
+
+# Store globally so it is available in all routes
+$currencies = parsed_data.fetch("currencies")
+$currencies_keys = $currencies.keys
+
+# Homepage route
 get("/") do
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
-
-  # Use HTTP.get to retrieve the API data
-  @raw_response = HTTP.get(api_url)
-
-  # Get the body of the response as a string
-  @raw_string = @raw_response.to_s
-
-  # Convert the string to JSON
-  @parsed_data = JSON.parse(@raw_string)
-
-  @currencies = @parsed_data.fetch("currencies")
-  @currencies_keys = @currencies.keys
-
+  @currencies_keys = $currencies_keys # Assign global data to instance variable
   erb(:homepage)
 end
 
+# Route for a specific currency
 get("/:currency") do
   @currency = params.fetch("currency")
-  # api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
-  
-  # # Fetch API data (again)
-  # raw_response = HTTP.get(api_url)
-  # parsed_data = JSON.parse(raw_response.to_s)
-
-  # currencies = parsed_data.fetch("currencies")
-  # currency_key = params[:currency] # Get currency from URL
-
+  @currencies_keys = $currencies_keys # Assign global data
   erb(:currency)
 end
