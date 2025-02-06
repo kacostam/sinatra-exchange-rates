73813037b4058ab2b4b3e18f991bdaf72155a64f
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-06 05:17:23 +0000

    enabled currency listing and dynamic path for each

--- app.rb
+++ app.rb
@@ -1,9 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
 
+# Pull in the HTTP class
+require "http"
+
+# define a route for the homepage
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+  # Use HTTP.get to retrieve the API data
+  @raw_response = HTTP.get(api_url)
+
+  # Get the body of the response as a string
+  @raw_string = @raw_response.to_s
+
+  # Convert the string to JSON
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+  @currencies_keys = @currencies.keys
+
+  erb(:homepage)
+end
+
+get("/:currency") do
+  @currency = params.fetch("currency")
+  # api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  
+  # # Fetch API data (again)
+  # raw_response = HTTP.get(api_url)
+  # parsed_data = JSON.parse(raw_response.to_s)
+
+  # currencies = parsed_data.fetch("currencies")
+  # currency_key = params[:currency] # Get currency from URL
+
+  erb(:currency)
 end
